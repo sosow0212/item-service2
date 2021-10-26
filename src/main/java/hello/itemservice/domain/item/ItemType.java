f4f3486486c544d53cfdf3af094bdef938a71af0
sosow0212 <sosow0212@naver.com>
2021-10-26 20:37:25 +0900

    라디오 박스

--- src/main/java/hello/itemservice/domain/item/ItemType.java
+++ src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -1,12 +1,11 @@
 package hello.itemservice.domain.item;
-
 public enum ItemType {
-
-    BOOK("도서"), FOOD("음식"), ETC("기타");
-
+    BOOK("도서"), FOOD("식품"), ETC("기타");
     private final String description;
-
     ItemType(String description) {
         this.description = description;
+    }
+    public String getDescription() {
+        return description;
     }
 }
